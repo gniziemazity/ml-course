e0c264968baf76535b7f572d912be73d8d74d157
Radu Mariescu-Istodor <gniziemazity@gmail.com>
2023-03-24 11:53:21 +0200

    Add files via upload

---
+++ P7.model_evaluation_lesson/common/constants.js
@@ -0,0 +1,21 @@
+const constants={};
+
+constants.DATA_DIR="../data";
+constants.RAW_DIR=constants.DATA_DIR+"/raw";
+constants.DATASET_DIR=constants.DATA_DIR+"/dataset";
+constants.JSON_DIR=constants.DATASET_DIR+"/json";
+constants.IMG_DIR=constants.DATASET_DIR+"/img";
+constants.SAMPLES=constants.DATASET_DIR+"/samples.json";
+constants.FEATURES=constants.DATASET_DIR+"/features.json";
+constants.TRAINING=constants.DATASET_DIR+"/training.json";
+constants.TESTING=constants.DATASET_DIR+"/testing.json";
+constants.JS_OBJECTS="../common/js_objects";
+constants.SAMPLES_JS=constants.JS_OBJECTS+"/samples.js";
+constants.FEATURES_JS=constants.JS_OBJECTS+"/features.js";
+constants.TRAINING_JS=constants.JS_OBJECTS+"/training.js";
+constants.TESTING_JS=constants.JS_OBJECTS+"/testing.js";
+constants.MIN_MAX_JS=constants.JS_OBJECTS+"/minMax.js";
+
+if(typeof module!=='undefined'){
+   module.exports=constants;
+}
