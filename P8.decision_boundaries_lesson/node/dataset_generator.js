7af83362b576ce07255152a6d7d944aa0fe7e183
eugenmihailescux@gmail.com <eugenmihailescux@gmail.com>
2023-07-12 23:23:31 +0200

    fixed issue #1

--- P8.decision_boundaries_lesson/node/dataset_generator.js
+++ P8.decision_boundaries_lesson/node/dataset_generator.js
@@ -58,6 +58,7 @@ fs.writeFileSync(constants.SAMPLES,
    JSON.stringify(samples)
 );
 
+fs.mkdirSync(constants.JS_OBJECTS, {recursive: true});
 fs.writeFileSync(constants.SAMPLES_JS,
    "const samples="+JSON.stringify(samples)+";"
 );
